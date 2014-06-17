a6c00638b3d466a61e3381a98e6b44cf2d085164
Zarah Dominguez <zdominguez@cloudsherpas.com>
2014-06-17 16:34:50 +0800

    Removed dependency on ButterKnife.

--- src/com/blogspot/droidista/swiperefreshtest/MainActivity.java
+++ src/com/blogspot/droidista/swiperefreshtest/MainActivity.java
@@ -9,16 +9,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 public class MainActivity extends Activity {
 
